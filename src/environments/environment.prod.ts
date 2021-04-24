6a1b13c44280fa1699a4f8332733827cc5ca7cdb
NickAponte <apontenick@aol.com>
2021-04-24 18:38:18 -0400

    another try

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  bitInfoDBHost : 'https://shrouded-atoll-63514.herokuapp.com/',
+  newsApiKey : '874a01369695467f872769bf97b6cdb8'
 };
