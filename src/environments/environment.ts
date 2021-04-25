626af10a682d847b48f2118f5b816e7b04be8d62
NickAponte <apontenick@aol.com>
2021-04-25 16:53:11 -0400

    so dang close i can smell it

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  bitInfoDBHost : 'https://shrouded-atoll-63514.herokuapp.com/',
+  bitInfoDBHost : 'https://shrouded-atoll-63514.herokuapp.com',
   newsApiKey : '874a01369695467f872769bf97b6cdb8'
 };
 
